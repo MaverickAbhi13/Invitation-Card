2bb24efbdecccb121a11b0c03e820b625d3f9b15
MaverickAbhi13 <141sharma.abhi@gmail.com>
2026-08-03 01:22:56 +0530

    Create Create script.js

---
+++ Create script.js
@@ -0,0 +1,12 @@
+document.getElementById("loader").style.display="none";
+
+document.getElementById("bride").innerHTML=wedding.bride;
+
+document.getElementById("groom").innerHTML=wedding.groom;
+
+document.getElementById("openBtn")
+.addEventListener("click",()=>{
+
+alert("Envelope animation will open here in Chapter 2.");
+
+});
